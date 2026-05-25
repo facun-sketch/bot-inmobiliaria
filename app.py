4dc68800b7ad9502d6a8b097fb9c4475bf8c2037
Facundo Ruiz <facundo.ruizesp@gmail.com>
2026-05-25 16:14:09 -0300

    Fix send_file import

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 
-from flask import Flask, request, jsonify, send_from_directory
+from flask import Flask, request, jsonify, send_from_directory, send_file
 from flask_cors import CORS
 from openai import OpenAI
 from twilio.twiml.messaging_response import MessagingResponse
